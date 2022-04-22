2d201a9014e7fff2f551cd0a33ccf4dcf80ae89b
Luis Cossío <luis.cossio@outlook.com>
2022-04-21 22:37:06 -0500

    added expressions

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,7 @@
 use std::fmt::Display;
 
 use chumsky::{
-    prelude::{filter, filter_map, just, one_of, skip_then_retry_until, take_until, recursive, Simple},
+    prelude::{filter, filter_map, just, one_of, skip_then_retry_until, take_until, recursive, Simple, Recursive},
     text::{self, TextParser},
     Parser, select, recursive,
 };
@@ -14,8 +14,8 @@ type Spanned<T> = (T, Span);
 pub enum Stmt {
     Declaration(Decl),
     Assignment {
-        ident: String, 
-        expr: Spanned<Expr>,
+        id: String, 
+        value: Spanned<Expr>,
     },
     // Condition(Spanned<Cond>),
     Loop(Loop),
@@ -27,7 +27,7 @@ pub enum Decl {
     Let { 
         id: String,
         type_: Type,
-        value: Option<Expr>,
+        value: Option<Spanned<Expr>>,
     },
     Fun {
         id: String,
@@ -81,7 +81,7 @@ pub enum BinOp {
     // Mod,
     //// Comparison
     Eq,
-    Neq,
+    Ne,
     Lt,
     Gt,
     Lte,
@@ -155,39 +155,44 @@ fn parser() -> impl Parser<Token, Vec<Spanned<Stmt>>, Error = Simple<Token>> {
     
     let expr = recursive(|expr| {
         let ident = ident.clone()
-            .map(|name| Expr::Ident(name));
+            .map_with_span(|name, span| (Expr::Ident(name), span));
             
         let number = filter_map(|span: Span, tok| {
-            match tok {
-                Token::Int(i) => Ok(Expr::Constant(Literal::Int(i.parse().unwrap()))),
-                Token::Float(f) => Ok(Expr::Constant(Literal::Float(f.parse().unwrap()))),
-                _ => Err(Simple::custom(span, "Not a number, expected int or float")),
-            }
-        }).labelled("number");
+                match tok {
+                    Token::Int(i) => Ok(Expr::Constant(Literal::Int(i.parse().unwrap()))),
+                    Token::Float(f) => Ok(Expr::Constant(Literal::Float(f.parse().unwrap()))),
+                    _ => Err(Simple::custom(span, "Not a number, expected int or float")),
+                }
+            }).map_with_span(|expr, span| (expr, span))
+            .labelled("number");
 
-
-        let literal = select! {
+        let value = select! {
             Token::Bool(b) => Expr::Constant(Literal::Bool(b)),
             Token::Char(c) => Expr::Constant(Literal::Char(c)),
             Token::Str(s) => Expr::Constant(Literal::String(s)),
-        };
+        }
+        .map_with_span(|expr, span| (expr, span))
+        .or(number);
         
-        let literal = literal
-            .or(number)
-            .labelled("literal");
-
-        let item = expr.clone()
-            .map_with_span(|item, span: Span| (item, span))
+        let item = expr
+            .clone()
             .separated_by(just(Token::Ctrl(',')))
             .allow_trailing()
             .labelled("item");
-        
-        let array = item.clone()
-            // .repeated()
+            
+        let array = item
+            .clone()
             .delimited_by(just(Token::Ctrl('[')), just(Token::Ctrl(']')))
-            .map(|items| Expr::Array(items))
+            .map_with_span(|items, span| (Expr::Array(items), span))
             .labelled("array");
-
+            
+        let atom = value
+            .or(ident)
+            .or(array)
+            .or(expr
+                .clone()
+                .delimited_by(just(Token::Ctrl('(')), just(Token::Ctrl(')')))
+            );
         // let args = item
         //         .delimited_by(just(Token::Ctrl('(')), just(Token::Ctrl(')')))
         //         .map_with_span(|(arg, span)| (arg, span))
@@ -205,11 +210,77 @@ fn parser() -> impl Parser<Token, Vec<Spanned<Stmt>>, Error = Simple<Token>> {
         //         .repeated()
         //     )
         //     .foldl(| |);
+        
+        let op = just(Token::Op(Op::Mul))
+            .to(BinOp::Mul)
+            .or(just(Token::Op(Op::Div))
+                .to(BinOp::Div));
 
-        literal
-            .or(array)
-            .or(ident)
-            .labelled("expr")
+        let product = atom
+            .clone()
+            .then(op.then(atom).repeated())
+            .foldl(|a, (op, b)| {
+                let span = a.1.start..b.1.end;
+                (Expr::Binary {
+                    lhs: Box::new(a), 
+                    op, 
+                    rhs: Box::new(b)
+                }, span)
+            });
+
+        let op = just(Token::Op(Op::Add))
+            .to(BinOp::Add)
+            .or(just(Token::Op(Op::Sub))
+                .to(BinOp::Sub));
+        let sum = product
+            .clone()
+            .then(op.then(product).repeated())
+            .foldl(|a, (op, b)| {
+                    let span = a.1.start..b.1.end;                
+                    (Expr::Binary {
+                        lhs: Box::new(a), 
+                        op, 
+                        rhs: Box::new(b)
+                    }, span)
+                }
+            );
+        
+        let op = filter_map( |span, tok| match tok {
+                Token::Op(Op::Eq) => Ok(BinOp::Eq),
+                Token::Op(Op::Ne) => Ok(BinOp::Ne),
+                Token::Op(Op::Lt) => Ok(BinOp::Lt),
+                Token::Op(Op::Gt) => Ok(BinOp::Gt),
+                Token::Op(Op::Lte) => Ok(BinOp::Lte),
+                Token::Op(Op::Gte) => Ok(BinOp::Gte),
+                _ => Err(Simple::custom(span, "Not a comparison operator")),
+            });        
+        let comparison = sum
+            .clone()
+            .then(op.then(sum).repeated())
+            .foldl(|a, (op, b)| {
+                let span = a.1.start..b.1.end;
+                (Expr::Binary {
+                    lhs: Box::new(a), 
+                    op, 
+                    rhs: Box::new(b)
+                }, span)
+            });
+        
+        let op = just(Token::Op(Op::And)).to(BinOp::And)
+            .or(just(Token::Op(Op::Or)).to(BinOp::Or));
+        let boolean = comparison
+            .clone()
+            .then(op.then(comparison).repeated())
+            .foldl(|a, (op, b)| {
+                let span = a.1.start..b.1.end;
+                (Expr::Binary {
+                    lhs: Box::new(a), 
+                    op, 
+                    rhs: Box::new(b)
+                }, span)
+            });
+
+        boolean
 
     });
     
@@ -247,18 +318,29 @@ fn parser() -> impl Parser<Token, Vec<Spanned<Stmt>>, Error = Simple<Token>> {
         .then_ignore(just(Token::Ctrl(':')))
         .then(type_);
         
-    let let_assign = let_decl.clone()
-        .then_ignore(just(Token::Op(Op::Assign)))
-        .then(expr);
         
+    let assign_rhs = just(Token::Op(Op::Assign))
+        .ignore_then(expr);
+
+    let let_assign = let_decl.clone()
+        .then(assign_rhs.clone());
+
     let let_ = let_assign
-        .map(|((id, type_), value)| Stmt::Declaration(Decl::Let { id, type_, value: Some(value) }))
+        .map(|((id, type_), value)| 
+            Stmt::Declaration(Decl::Let { id, type_, value: Some(value) }))
         .or(let_decl
-            .map(|(id, type_)| Stmt::Declaration(Decl::Let { id, type_, value: None })));
+            .map(|(id, type_)| 
+                Stmt::Declaration(Decl::Let { id, type_, value: None })));
+
+    let assign = ident
+        .then(assign_rhs.clone())
+        .map(|(id, value)| 
+            Stmt::Assignment { id, value });
     
     let decl = let_;
-
+    
     let stmt = decl
+        .or(assign)
         .then_ignore(just(Token::Ctrl(';')));
 
     stmt.recover_with(skip_then_retry_until([]))
@@ -280,10 +362,10 @@ mod tests {
         let tokens = tokens.unwrap();
 
         let len = input.chars().count();
-
+        
         let (stmts, parse_errs) = parser().parse_recovery(Stream::from_iter(len..len + 1, tokens.into_iter()));
         
-        println!("{:?}", parse_errs);
+        println!("errors: {:?}", parse_errs);
 
         stmts.unwrap()
     }
@@ -469,7 +551,12 @@ while (a < 10) {
     #[test]
     fn test_asgmt() {
         let src = r#"
-let a: int = 1; let b: float = 2.0; let c: bool = true; let d: char = 'a'; let e: string = "hello"; let f: [int] = [1, 2, 3, 4, 5];
+let a: int = 1;
+let b: float = 2.0;
+let c: bool = true;
+let d: char = 'a';
+let e: string = "hello";
+let f: [int] = [1, 2, 3, 4, 5];
 "#;
 
         let stmts = parse_from(src);
@@ -480,7 +567,7 @@ let a: int = 1; let b: float = 2.0; let c: bool = true; let d: char = 'a'; let e
                 Stmt::Declaration(Decl::Let {
                     id: "a".to_string(),
                     type_: Type::Int,
-                    value: Some(Expr::Constant(Literal::Int(1)))
+                    value: Some((Expr::Constant(Literal::Int(1)), 14..15))
                 }),
                 1..16
             ) 
@@ -492,7 +579,7 @@ let a: int = 1; let b: float = 2.0; let c: bool = true; let d: char = 'a'; let e
                 Stmt::Declaration(Decl::Let {
                     id: "b".to_string(),
                     type_: Type::Float,
-                    value: Some(Expr::Constant(Literal::Float(2.0)))
+                    value: Some((Expr::Constant(Literal::Float(2.0)), 32..35))
                 }),
                 17..36
             ) 
@@ -504,7 +591,7 @@ let a: int = 1; let b: float = 2.0; let c: bool = true; let d: char = 'a'; let e
                 Stmt::Declaration(Decl::Let {
                     id: "c".to_string(),
                     type_: Type::Bool,
-                    value: Some(Expr::Constant(Literal::Bool(true)))
+                    value: Some((Expr::Constant(Literal::Bool(true)), 51..55))
                 }),
                 37..56
             ) 
@@ -516,7 +603,7 @@ let a: int = 1; let b: float = 2.0; let c: bool = true; let d: char = 'a'; let e
                 Stmt::Declaration(Decl::Let {
                     id: "d".to_string(),
                     type_: Type::Char,
-                    value: Some(Expr::Constant(Literal::Char('a')))
+                    value: Some((Expr::Constant(Literal::Char('a')), 71..74))
                 }),
                 57..75
             ) 
@@ -528,7 +615,7 @@ let a: int = 1; let b: float = 2.0; let c: bool = true; let d: char = 'a'; let e
                 Stmt::Declaration(Decl::Let {
                     id: "e".to_string(),
                     type_: Type::String,
-                    value: Some(Expr::Constant(Literal::String("hello".to_string())))
+                    value: Some((Expr::Constant(Literal::String("hello".to_string())), 92..99))
                 }),
                 76..100
             ) 
@@ -540,13 +627,13 @@ let a: int = 1; let b: float = 2.0; let c: bool = true; let d: char = 'a'; let e
                 Stmt::Declaration(Decl::Let {
                     id: "f".to_string(),
                     type_: Type::Array(Box::new(Type::Int)),
-                    value: Some(Expr::Array(vec![
+                    value: Some((Expr::Array(vec![
                         (Expr::Constant(Literal::Int(1)), 117..118),
                         (Expr::Constant(Literal::Int(2)), 120..121),
                         (Expr::Constant(Literal::Int(3)), 123..124),
                         (Expr::Constant(Literal::Int(4)), 126..127),
                         (Expr::Constant(Literal::Int(5)), 129..130),
-                    ]))
+                    ]), 116..131))
                 }),
                 101..132
             ) 
@@ -556,23 +643,91 @@ let a: int = 1; let b: float = 2.0; let c: bool = true; let d: char = 'a'; let e
     #[test]
     fn test_arithmetic_exp() {
         let src = "
-let a: int = 1 + 2;
-let b: int = 1 + 2 + 3;
-let c: int = 1 - a * b / 2 + 2;
-let d: int = (1 - a) * b / (2 + 2) + 3;
+a = 1 + 2;
+b = 1 + 2 + 3;
+c = 1 - a * b / 2 + 2;
+d = (1 - a) * b / (2 + 2) + 3;
 ";
 
-        todo!();
+        let stmts = parse_from(src);
+        println!("{:?}", stmts);
+        // (1 + 2)
+        assert_eq!(
+            stmts[0],
+            (
+                Stmt::Assignment {
+                    id: "a".to_string(),
+                    value: (Expr::Binary {
+                        lhs: Box::new((Expr::Constant(Literal::Int(1)), 5..6)), 
+                        op: BinOp::Add, 
+                        rhs: Box::new((Expr::Constant(Literal::Int(2)), 9..10))
+                    }, 5..10)
+                },
+                1..11
+            ) 
+        );
+
+        // ((1 + 2) + 3)
+        assert_eq!(
+            stmts[1],
+            (
+                Stmt::Assignment {
+                    id: "b".to_string(),
+                    value: (Expr::Binary {
+                        lhs: Box::new((Expr::Binary {
+                            lhs: Box::new((Expr::Constant(Literal::Int(1)), 16..17)), 
+                            op: BinOp::Add, 
+                            rhs: Box::new((Expr::Constant(Literal::Int(2)), 20..21)), 
+                        }, 16..21)),
+                        op: BinOp::Add, 
+                        rhs: Box::new((Expr::Constant(Literal::Int(3)), 24..25))
+                    }, 16..25),
+                },
+                12..26
+            ) 
+        );
+
+
+        // ((1 - ((a * b) / 2)) + 2)
+        assert_eq!(
+            stmts[2],
+            (
+                (Stmt::Assignment {
+                    id: "c".to_string(),
+                    value: (Expr::Binary {
+                        lhs: Box::new((Expr::Binary {
+                            lhs: Box::new((Expr::Constant(Literal::Int(1)), 31..32)), 
+                            op: BinOp::Sub, 
+                            rhs: Box::new((Expr::Binary {
+                                    lhs: Box::new((Expr::Binary {
+                                        lhs: Box::new((Expr::Ident("a".to_string()), 35..36)), 
+                                        op: BinOp::Mul, 
+                                        rhs: Box::new((Expr::Ident("b".to_string()), 39..40)), 
+                                    }, 35..40)),
+                                    op: BinOp::Div,
+                                    rhs: Box::new((Expr::Constant(Literal::Int(2)), 43..44)), 
+                                }, 35..44)),
+                        }, 31..44)),
+                        op: BinOp::Add,
+                        rhs: Box::new((Expr::Constant(Literal::Int(2)), 47..48)),
+                    },31..48),
+                }, 27..49))
+            );
+
+        // ((((1 - a) * b) / (2 + 2)) + 3)
+        
+
+        // todo!();
     }
 
     #[test]
     fn test_boolean_exp() {
         let src = "
-let a: bool = true;
-let b: bool = false;
-let c: bool = a && b;
-let d: bool = a || b;
-let e: bool = !a;
+a = true;
+b = false;
+c = a && b;
+d = a || b;
+e = !a;
 ";
         todo!();
     }
