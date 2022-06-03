28fc288cb6d53558868535003f986026f5a38241
Luis Cossío <luis.cossio@outlook.com>
2022-06-02 21:21:30 -0500

    hotfix: improve `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,15 @@ pub mod utils;
 pub(crate) mod shared;
 pub mod vm;
 
+use std::env;
+
 pub use crate::{compiler::compile, vm::program::Program};
 
 
 fn main() {
-    // let src = fs::read_to_string(env::args().nth(1).expect("Expected file argument"))
-    //     .expect("Failed to read file");
+    let filename = env::args().nth(1).unwrap_or("examples/simple.gu".to_string());
 
-    // let filename = "examples/simple.gu";
-    // let src = fs::read_to_string(filename.clone())
-    //     .expect("Failed to read file");
-
-    let filename = "examples/simple.gu";
-    let output = compiler::compile(filename);
+    let output = compiler::compile(&filename);
 
     if let Some(output) = output {
         println!("guccic successfully compiled {} to {}", filename, output);
